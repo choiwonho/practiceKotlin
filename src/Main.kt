a156955f2e9eb78aad72b7cae7a2be4029bb0fed
choiwonho <jsa1031109@naver.com>
2021-03-30 01:33:28 +0900

    First Commit
    Add Main.kt for practice for Kotlin language and using git

---
+++ src/Main.kt
@@ -0,0 +1,11 @@
+val name = "Nate"
+var greeting: String? = null
+
+fun main() {
+    greeting = "Hello"
+    when (greeting) {
+        null -> println("Hi")
+        else -> println(greeting)
+    }
+    println(name)
+}
